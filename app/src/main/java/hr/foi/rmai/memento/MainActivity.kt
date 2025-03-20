63312bad764f67b4c736d12de473c0ca051e6c62
Lovro <lovro.posaric@gmail.com>
2025-03-20 06:55:33 +0100

    Setup initial connection between fragments and the fragment container

--- app/src/main/java/hr/foi/rmai/memento/MainActivity.kt
+++ app/src/main/java/hr/foi/rmai/memento/MainActivity.kt
@@ -5,12 +5,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import hr.foi.rmai.memento.adapters.MainPagerAdapter
 
 class MainActivity : AppCompatActivity() {
+    lateinit var tabLayout: TabLayout
+    lateinit var viewPager: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        setupQualityOfLifeImprovements()
+
+        tabLayout = findViewById(R.id.tabs)
+        viewPager = findViewById(R.id.viewPager)
+        viewPager.adapter = MainPagerAdapter(supportFragmentManager, lifecycle)
+    }
+
+    private fun setupQualityOfLifeImprovements() {
+        enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
