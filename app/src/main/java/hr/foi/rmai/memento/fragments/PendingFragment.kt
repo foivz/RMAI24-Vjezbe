5da4050597b711cf981b8cab52a2d24bfa8a451f
Lovro <lovro.posaric@gmail.com>
2025-03-21 15:03:55 +0100

    Establish the connection between mock data and the task list view

--- app/src/main/java/hr/foi/rmai/memento/fragments/PendingFragment.kt
+++ app/src/main/java/hr/foi/rmai/memento/fragments/PendingFragment.kt
@@ -1,24 +1,31 @@
 package hr.foi.rmai.memento.fragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import hr.foi.rmai.memento.R
+import hr.foi.rmai.memento.adapters.TasksAdapter
 import hr.foi.rmai.memento.helpers.MockDataLoader
 
 class PendingFragment : Fragment() {
     private val mockData = MockDataLoader.getMockData()
 
+    private lateinit var recylerView: RecyclerView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        mockData.forEach {
-            Log.i("RMAI-MOCK", it.name)
-        }
         return inflater.inflate(R.layout.fragment_pending, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recylerView = view.findViewById(R.id.rv_pending_tasks)
+        recylerView.adapter = TasksAdapter(mockData)
+        recylerView.layoutManager = LinearLayoutManager(view.context)
     }
 }
