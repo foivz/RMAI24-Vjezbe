72b858f132b3072f2dc9655f07296e0ec19899c9
Lovro <lovro.posaric@gmail.com>
2025-03-21 14:58:32 +0100

    Grab the mock data within a fragment and display all entries in the log

--- app/src/main/java/hr/foi/rmai/memento/fragments/PendingFragment.kt
+++ app/src/main/java/hr/foi/rmai/memento/fragments/PendingFragment.kt
@@ -1,17 +1,24 @@
 package hr.foi.rmai.memento.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import hr.foi.rmai.memento.R
+import hr.foi.rmai.memento.helpers.MockDataLoader
 
 class PendingFragment : Fragment() {
+    private val mockData = MockDataLoader.getMockData()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        mockData.forEach {
+            Log.i("RMAI-MOCK", it.name)
+        }
         return inflater.inflate(R.layout.fragment_pending, container, false)
     }
 }
