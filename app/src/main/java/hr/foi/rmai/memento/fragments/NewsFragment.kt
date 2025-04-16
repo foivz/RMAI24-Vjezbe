703415302126b359e1fd72ae75e09c5ff88bd98b
Lovro Posarić <lovro.posaric@gmail.com>
2025-04-16 10:53:50 +0200

    Display news in a RecyclerView on the NewsFragment

--- app/src/main/java/hr/foi/rmai/memento/fragments/NewsFragment.kt
+++ app/src/main/java/hr/foi/rmai/memento/fragments/NewsFragment.kt
@@ -6,7 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import hr.foi.rmai.memento.R
+import hr.foi.rmai.memento.adapters.NewsAdapter
 import hr.foi.rmai.memento.ws.NewsResponse
 import hr.foi.rmai.memento.ws.WsNews
 import retrofit2.Call
@@ -15,6 +19,7 @@ import retrofit2.Response
 
 class NewsFragment : Fragment() {
     private val ws = WsNews.newsService
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,24 +28,38 @@ class NewsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_news, container, false)
     }
 
+    private fun displayWebServiceErrorMessage() {
+        Toast.makeText(
+            context,
+            getString(R.string.news_ws_err_msg),
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
     private fun loadNews() {
         ws.getNews().enqueue(
             object : Callback<NewsResponse> {
                 override fun onResponse(call: Call<NewsResponse>, response: Response<NewsResponse>) {
-                    if (response?.isSuccessful == true) {
+                    if (response.isSuccessful) {
                         val responseBody = response.body()
-                        Log.i("RMAI-WS", responseBody?.count.toString())
+                        val news = responseBody!!.results
+                        recyclerView.adapter = NewsAdapter(news)
+                    } else {
+                        displayWebServiceErrorMessage()
                     }
                 }
 
                 override fun onFailure(call: Call<NewsResponse>?, t: Throwable?) {
-
+                    displayWebServiceErrorMessage()
                 }
             }
         )
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = view.findViewById(R.id.rv_news)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
         loadNews()
     }
 }
