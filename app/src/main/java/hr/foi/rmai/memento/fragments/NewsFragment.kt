76c3621779c844760b81837ddae7e008518b5bcf
Lovro Posarić <lovro.posaric@gmail.com>
2025-04-16 10:54:15 +0200

    Toggle parts of display while news are loading

--- app/src/main/java/hr/foi/rmai/memento/fragments/NewsFragment.kt
+++ app/src/main/java/hr/foi/rmai/memento/fragments/NewsFragment.kt
@@ -6,9 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import hr.foi.rmai.memento.R
 import hr.foi.rmai.memento.adapters.NewsAdapter
 import hr.foi.rmai.memento.ws.NewsResponse
@@ -20,6 +23,8 @@ import retrofit2.Response
 class NewsFragment : Fragment() {
     private val ws = WsNews.newsService
     private lateinit var recyclerView: RecyclerView
+    private lateinit var loadingCircle: ProgressBar
+    private lateinit var btnRefresh: FloatingActionButton
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,7 +41,15 @@ class NewsFragment : Fragment() {
         ).show()
     }
 
+    private fun toggleDisplay(isLoading: Boolean) {
+        btnRefresh.isVisible = !isLoading
+        recyclerView.isVisible = !isLoading
+        loadingCircle.isVisible = isLoading
+    }
+
     private fun loadNews() {
+        toggleDisplay(true)
+
         ws.getNews().enqueue(
             object : Callback<NewsResponse> {
                 override fun onResponse(call: Call<NewsResponse>, response: Response<NewsResponse>) {
@@ -47,16 +60,26 @@ class NewsFragment : Fragment() {
                     } else {
                         displayWebServiceErrorMessage()
                     }
+
+                    toggleDisplay(false)
                 }
 
                 override fun onFailure(call: Call<NewsResponse>?, t: Throwable?) {
                     displayWebServiceErrorMessage()
+                    toggleDisplay(false)
                 }
             }
         )
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        loadingCircle = view.findViewById(R.id.pb_news_fragment_loading)
+
+        btnRefresh = view.findViewById(R.id.fab_news_fragment_refresh_news)
+        btnRefresh.setOnClickListener {
+            loadNews()
+        }
+
         recyclerView = view.findViewById(R.id.rv_news)
         recyclerView.layoutManager = LinearLayoutManager(context)
 
