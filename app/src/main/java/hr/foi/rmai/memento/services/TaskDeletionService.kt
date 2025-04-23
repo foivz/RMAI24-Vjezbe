dc7e19c44251c383ba66ff1cff16f38c31abb8f9
Lovro Posarić <lovro.posaric@gmail.com>
2025-04-23 11:56:26 +0200

    Don't run the cleanup procedure on service start, instead do it when explicitly requested

--- app/src/main/java/hr/foi/rmai/memento/services/TaskDeletionService.kt
+++ app/src/main/java/hr/foi/rmai/memento/services/TaskDeletionService.kt
@@ -5,13 +5,14 @@ import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
 import hr.foi.rmai.memento.database.TasksDatabase
+import hr.foi.rmai.memento.helpers.TaskDeletionServiceHelper
 
 class TaskDeletionService : Service() {
     inner class TaskDeletionBinder : Binder() {
         fun getService(): TaskDeletionService = this@TaskDeletionService
     }
 
-    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+    fun deleteOldTasks(onTaskDeletion: (Int) -> Unit) {
         TasksDatabase.buildInstance(applicationContext)
 
         val tasksDao = TasksDatabase.getInstance().getTasksDao()
@@ -19,13 +20,10 @@ class TaskDeletionService : Service() {
         tasksDao.getAllTasks(true).forEach {
             if (it.isOverdue()) {
                 tasksDao.removeTask(it)
+                onTaskDeletion(it.id)
             }
         }
-
-        return START_REDELIVER_INTENT
     }
 
     override fun onBind(intent: Intent): IBinder? = TaskDeletionBinder()
-
-    }
 }
