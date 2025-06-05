2410f28538df89620151176d3f46ad758dd82e1a
Dario Ljubas <darioljubas33@gmail.com>
2025-06-05 10:24:33 +0200

    Added drone

---
+++ app/src/main/java/hr/foi/rmai/memento/entities/Drone.kt
@@ -0,0 +1,50 @@
+package hr.foi.rmai.memento.entities
+
+import android.graphics.PointF
+
+class Drone(worldStartX: Int, worldStartY: Int)
+    : GameObject(1f, 1f, 1, "drone", 'd') {
+    var lastWaypointSetTime: Long = 0
+    var currentWaypoint: PointF
+    val MAX_X_VELOCITY = 3f
+    val MAX_Y_VELOCITY = 3f
+
+    init {
+        moves = true
+        active = true
+        visible = true
+        currentWaypoint = PointF()
+        setWorldLocation(
+            worldStartX.toFloat(),
+            worldStartY.toFloat(),
+            0
+        )
+        updateRectHitbox()
+        facing = RIGHT
+    }
+
+    override fun update(fps: Int, gravity: Float) {
+        if (currentWaypoint.x > worldLocation.x) {
+            xVelocity = MAX_X_VELOCITY
+        } else if (currentWaypoint.x < worldLocation.x) {
+            xVelocity = -MAX_X_VELOCITY
+        } else {
+            xVelocity = 0f
+        }
+
+        if (currentWaypoint.y >= worldLocation.y) {
+            yVelocity = MAX_X_VELOCITY
+        } else if (currentWaypoint.y < worldLocation.y) {
+            yVelocity = -MAX_Y_VELOCITY
+        } else {
+            yVelocity = 0f
+        }
+
+        move(fps)
+        updateRectHitbox()
+    }
+}
+
+
+
+
